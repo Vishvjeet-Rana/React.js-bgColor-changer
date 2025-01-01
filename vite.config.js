3fde7df255c931e64f7318ceb7c703424717361c
Vishvjeet-Rana <aryanrana62366@gmail.com>
2025-01-01 12:45:58 +0530

    changes root to docs

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./", // Add a leading and trailing slash
+  build: {
+    outDir: "./docs", // Specify the output directory
+  },
 });
